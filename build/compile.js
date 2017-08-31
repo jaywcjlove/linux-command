e7483609605699423494ee5d3ebc9b49644b35a2
jaywcjlove <398188662@qq.com>
2017-08-31 15:02:46 +0800

    Markdown escape error.

--- build/compile.js
+++ build/compile.js
@@ -205,7 +205,10 @@ function ReadTmpToHTML(from_path,to_path,md_path,des_json, total_pages){
         marked(README_str.toString(),function(err,md_html){
             if (err) return console.log(error('  → '+md_path+" 转换成HTML失败!"));
 
-            html = html.replace('{{content}}',md_html);
+            html = html.split('{{content}}')
+            html.splice(1, 0, md_html);
+            html = html.join('')
+            // html = html.replace('{{content}}',md_html);
             fs.writeFileSync(new_to_path,html);
             console.log(success("  → ")+to_path + '');
         })
